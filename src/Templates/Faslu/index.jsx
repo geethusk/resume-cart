5a24c5fc1a5295e6fb84d982424cd2929adcd0b6
faslulfarisa <faslulfarisa5678@gmail.com>
2021-12-15 23:06:33 +0530

    jsx ongoing

--- src/Templates/Faslu/index.jsx
+++ src/Templates/Faslu/index.jsx
@@ -1,7 +1,45 @@
 const Faslu = () => {
     return (
-        <div>
-            Faslul Farisa
+        <div className="container">
+            <div className="left-section">
+                <h1>Chris Candidate</h1>
+                <h4>Sales Associate</h4>
+                <p>4759 Sunnydale Lane
+                    Plano, TX, 75071
+                    email@youremail.com
+                    (469) 385-2948</p>
+                <p>Human resources generalist with 8 years of experience in HR, including hiring and terminating, disciplining employees and helping department managers improve employee performance. Worked with labor unions to negotiate compensation packages for workers. Organized new hire training initiatives as well as ongoing training to adhere to workplace safety standards. Worked with OSHA to ensure that all safety regulations are followed.</p>
+                <h3>Key Skills</h3>
+                <li>Detail oriented</li>
+                <li>Well-versed in Texas employment law</li>
+                <li>Excellent written and oral communication skills</li> 
+                <li>Develops positive workplace relationships</li>
+            </div>
+            <div className="right-section">
+                <h3>Professional Experience</h3>
+                <li>Human Resources Manager
+                    Jim's Widget Factory, Plano, TX | January 2016 - Present
+                    <ul>
+                    <li>Implement effective company policies to ensure that all practices comply with labor and employment regulations</li>
+                    <li>Increased employee retention rates by managing workplace satisfaction to an over 90% success rate by creating and maintaining a positive work environment</li>
+                    <li>Develop targeted outreach practices to increase minority recruitment and ensure compliance with affirmative action policies</li>
+                    <li>Monitor scheduled in and out times as well as employee breaks to ensure that proper employment laws are met</li>
+                    </ul>
+                </li>
+                <li>Human Resources Associate
+                    Jim's Widget Factory, Plano, TX | March 2015 - January 2016
+                    <ul>
+                        <li>Implement effective company policies to ensure that all practices comply with labor and employment regulations</li>
+                        <li>Increased employee retention rates by managing workplace satisfaction to an over 90% success rate by creating and maintaining a positive work environment</li>
+                        <li>Develop targeted outreach practices to increase minority recruitment and ensure compliance with affirmative action policies</li>
+                    </ul>
+                </li>
+                <h3>Education</h3>
+                <p>September 2007 - May 2011
+                    Bachelor of Arts in Business
+                    The University of Texas at Dallas
+                    Academic Awardee of AY 2007-2008</p>
+            </div>
         </div>
     )
 }
