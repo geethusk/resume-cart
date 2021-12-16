baf212fe7031b9ec4016e55bbb030bfb919dac3f
Raeez98 <raeezmohammed97@gmail.com>
2021-12-16 13:39:20 +0530

    resume building

--- src/Templates/Raeez/index.jsx
+++ src/Templates/Raeez/index.jsx
@@ -1,8 +1,67 @@
+import "./raeez.css"
 const Raeez = () => {
     return (
-        <div>
-             Raeez mohammed 
+        <div className="main-body3">
+            <div className="header-section-3">
+                <div className="logo-3">CC</div>
+                <div className=" name-3">Chris Canditate
+                 <h2 className="job-3">Sales Associate</h2></div>
+                 <div className="headerdiscription-3">
+                      4759 Sunnydale Lane<br/>
+                    Plano, TX, 75071<br/>
+                   email@youremail.com<br/>
+                   (469) 385-2948<br/>
+                </div>
+            </div>
+            <div className="body-container-3">
+               <div className="body-des-3">Human resources generalist with 8 years of experience in HR, including hiring and terminating, disciplining employees and helping department managers improve employee performance. Worked with labor unions to negotiate compensation packages for workers. Organized new hire training initiatives as well as ongoing training to adhere to workplace safety standards. Worked with OSHA to ensure that all safety regulations are followed.</div>
+               <div className="firsthead-3">Professional Experience</div>
+               <div className="second-head-wrapper-3">
+               <div className="secondhead-3">Human Resources Manager</div>
+                  <div className="secondheaddis-3">Jim's Widget Factory, Plano, TX | January 2016 - Present</div>
+               
+               </div>
+               <ul>
+                  
+                   <li className="licolor-3"  >Implement effective company policies to ensure that all practices comply with labor and employment regulations</li>
+                   <li className="licolor-3">Increased employee retention rates by managing workplace satisfaction to an over 90% success rate by creating and maintaining a positive work environment</li>
+                   <li  className="licolor-3">Develop targeted outreach practices to increase minority recruitment and ensure compliance with affirmative action policies</li>
+                   <li  className="licolor-3">Monitor scheduled in and out times as well as employee breaks to ensure that proper employment laws are met</li>
+                  
+               </ul>
+               <div className="second-head-wrapper-3">
+               <div className="secondhead-3">Human Resources Manager</div>
+                  <div className="secondheaddis-3">Jim's Widget Factory, Plano, TX | January 2016 - Present</div>
+               
+               </div>
+               <ul>
+                  
+                   <li className="licolor-3"  >Implement effective company policies to ensure that all practices comply with labor and employment regulations</li>
+                   <li className="licolor-3">Increased employee retention rates by managing workplace satisfaction to an over 90% success rate by creating and maintaining a positive work environment</li>
+                   <li  className="licolor-3">Develop targeted outreach practices to increase minority recruitment and ensure compliance with affirmative action policies</li>
+                   <li  className="licolor-3">Monitor scheduled in and out times as well as employee breaks to ensure that proper employment laws are met</li>
+                  
+               </ul>
+               <div className="firsthead-3">Education</div>
+               <div className="second-head-wrapper-3">
+               <div className="secondhead-3">Bachelor of Arts in Business</div>
+                  <div className="secondheaddis-3">The University of Texas at Dallas | September 2007 - May 2011</div>
+               
+               </div>
+               <ul>
+                   <li className="licolor-3">Academic Awardee of AY 2007-2008</li>
+               </ul>
+               <div className="firsthead-3">Key Skills</div>
+               <ul>
+                   <li className="licolor-3">Detail oriented</li>
+                   <li className="licolor-3">Well-versed in Texas employment law</li>
+                   <li className="licolor-3">Excellent written and oral communication skill</li>
+                   <li className="licolor-3">Develops positive workplace relationships</li>
+               </ul>
 
+
+               
+            </div>
         </div>
     )
 }
