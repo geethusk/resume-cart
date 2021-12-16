880e3c3ffab3141f211a9f3ccbba05a7dc766941
Rajasree <rajasreepradhakrishnan@gmail.com>
2021-12-16 13:39:20 +0530

    resume416/12

--- src/Templates/Rajasree/index.jsx
+++ src/Templates/Rajasree/index.jsx
@@ -1,7 +1,70 @@
+import "./raju.css"
 const Rajasree = () => {
     return (
-        <div>
-            heyyy
+        <div className="main_body4">
+            <div className="header_section4">
+            <div className="logo4">CC</div>
+            <div className="name4">Chris Candidate
+            <h2 className="job4">Sales Associate</h2>
+            </div>
+            <div className="headerdescription4">
+                4759 Sunnydale Lane<br/>
+                Plano, TX, 75071<br/>
+                email@youremail.com<br/>
+                (469) 385-2948<br/>
+            </div>
+            </div>
+            <div className="body_description4">
+                <div className="firstdes4">
+            Human resources generalist with 8 years of experience in HR,
+             including hiring and terminating, 
+             disciplining employees and helping department managers improve employee performance. 
+             Worked with labor unions to negotiate compensation packages for workers.
+            Organized new hire training initiatives as well as ongoing training to adhere to workplace safety standards.
+             Worked with OSHA to ensure that all safety regulations are followed.
+            </div>
+            <div className="firsthead4">Professional Experience</div>
+            <div className="secondheadwrapper4">
+            <div className="secondhead4">Human Resources Manager</div>
+            <div className="secondsmallhead4">Jim's Widget Factory, Plano, TX | January 2016 - Present
+            </div></div>
+            <ul>
+                <li className="licolor">Implement effective company policies to ensure that all practices comply with labor and employment regulations</li>
+                <li className="licolor">Increased employee retention rates by managing workplace satisfaction to an over 90% success rate by creating and maintaining a positive work environment</li>
+                <li className="licolor">Develop targeted outreach practices to increase minority recruitment and ensure compliance with affirmative action policies</li>
+                <li className="licolor">Monitor scheduled in and out times as well as employee breaks to ensure that proper employment laws are met</li>
+            </ul>
+            <div className="secondheadwrapper4">
+            <div className="secondhead4">Human Resources Associate</div>
+            <div className="secondsmallhead4">Jim's Widget Factory, Plano, TX | March 2015 - January 2016
+            </div></div>
+            <ul>
+                <li className="licolor">Implement effective company policies to ensure that all practices comply with labor and employment regulations</li>
+                <li className="licolor">Increased employee retention rates by managing workplace satisfaction to an over 90% success rate by creating and maintaining a positive work environment</li>
+                <li className="licolor">Develop targeted outreach practices to increase minority recruitment and ensure compliance with affirmative action policies</li>
+                
+            </ul>
+            <div className="firsthead4">Education</div>
+            <div className="secondheadwrapper4">
+            <div className="secondhead4">Bachelor of Arts in Business</div>
+            <div className="secondsmallhead4">The University of Texas at Dallas | September 2007 - May 2011
+            </div></div>
+            <ul>
+                <li className="licolor">Academic Awardee of AY 2007-2008</li>
+               
+            </ul>
+            <div className="firsthead4">Key Skills</div>
+            <ul>
+                <li className="licolor">Detail oriented</li>
+                <li className="licolor">Well-versed in Texas employment law</li>
+                <li className="licolor">Excellent written and oral communication skills</li>
+                <li className="licolor">Develops positive workplace relationships</li>
+                
+            </ul>
+            
+            </div>
+            
+            
         </div>
     )
 }
