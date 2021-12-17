d6ca668ac681d0917dbe5614cf5d88f11e78861d
Raeez98 <raeezmohammed97@gmail.com>
2021-12-17 14:15:00 +0530

    template destructuring

--- src/Templates/Raeez/index.jsx
+++ src/Templates/Raeez/index.jsx
@@ -1,12 +1,13 @@
 import "./raeez.css"
 import { useState } from "react"
+import  produce from "immer"
 const Raeez = () => {
     const[template,setTemplate]=useState({
         theme:['#17a9e4','red'],
         logo:"CC",
-        name:"Raeez Canditate",
+        name:"Raeez Mohammed",
         designation:"Full Stack developer",
-        address:{street:"4759 Sunnydale Lane",  pincode:"Plano ,TX 75071",gmail:"email@youremail.com",phonenumber:"(469) 385-2948"},
+        address:{street:"4759 Sunnydale Lane",city:"Plano ,TX ",  pincode:"75071",gmail:"email@youremail.com",phonenumber:"(469) 385-2948"},
         bio:"Human resources generalist with 8 years of experience in HR, including hiring and terminating, disciplining employees and helping department managers improve employee performance. Worked with labor unions to negotiate compensation packages for workers. Organized new hire training initiatives as well as ongoing training to adhere to workplace safety standards. Worked with OSHA to ensure that all safety regulations are followed..",
         profexp:[
             {
@@ -50,38 +51,108 @@ const Raeez = () => {
         ]
     })
 
+    const changeState=(keys,value)=>{
+        setTemplate(
+            (prev)=> produce(prev,(draft)=>{
+                    switch(keys.length){
+                        case 1:
+                            draft[keys[0]]=value;
+                            break;
+                        case 2:
+                            draft[keys[0]][keys[1]]=value;
+                            break;
+                        case 3:
+                            draft[keys[0]][keys[1]][keys[2]]=value;
+                            break;
+                        case 4:
+                            draft[keys[0]][keys[1]][keys[2]][keys[3]]=value;   
+                            break;
+
+                        default:
+                             break;  
+                    }          
+                    
+                })
+        )
+    }
+
     const{theme,logo,name,designation,address,bio,profexp,Education,skills}=template;
-    const{street,pincode,gmail,phonenumber}=address;
+    const{street,pincode,gmail,phonenumber,city}=address;
     return (
         <div className="main-body3">
             <div className="header-section-3">
                 <div className="logo-3">{logo}</div>
-                <div className=" name-3" >{name}
-                 <h2 className="job-3">{designation}</h2></div>
+                <div className=" name-3" >
+                    <input type="text"
+                    value={name}
+                    className=" name-3"
+                    onChange={(e)=>changeState(["name"],e.target.value)}/>
+                  <h2 className="job-3"><input type="text" className="job-3" value={designation}
+                   onChange={(e)=>changeState(["designation"],e.target.value)}/>
+                  </h2></div>
                  <div className="headerdiscription-3">
-                  {street}<br/>
-                   {pincode}<br/>
-                   {gmail}<br/>
-                   {phonenumber}<br/>
+                  <input type="text"
+                  value= {street}
+                  className="headerdiscription-3"
+                  onChange={(e)=>changeState(["address","street"],e.target.value)}
+                  /><br/>
+                   <input type ="text"
+                   value={city}
+                   className="headerdiscription-3"
+                   onChange={(e)=>changeState(["address","city"],e.target.value)}/>
+                   <input type="text"
+                   value={pincode}
+                   onChange={(e)=>changeState(["address","pincode"],e.target.value)}
+                   /><br/>
+                   <input type="text"
+                   value={gmail}
+                   className="headerdiscription-3"
+                   onChange={(e)=>changeState(["address","gmail"],e.target.value)}/>
+                   <br/>
+                   <input type ="text"
+                    value={phonenumber}
+                    className="headerdiscription-3"
+                    onChange={(e)=>changeState(["address","phonenumber"],e.target.value)}/><br/>
                 </div>
             </div>
             <div className="body-container-3">
-               <div className="body-des-3">{bio}</div>
+               <div className="body-des-3">
+                   <textarea  value={bio}  onChange={(e)=>changeState(["bio"],e.target.value)}/> 
+                   </div>
                <div className="firsthead-3">Professional Experience</div>
                {profexp.map(({startingdate,endingdate,companyName,jobdesignation,detailsList},i)=>{
                    return(
                        <>
               
                <div className="second-head-wrapper-3">
-               <div className="secondhead-3">{jobdesignation}</div>
-                  <div className="secondheaddis-3">{companyName} | {startingdate} - {endingdate}</div>
+               <div className="secondhead-3">
+                   <input type="text"
+                    className="secondhead-3"
+                   value={jobdesignation}
+                   onChange={(e)=>changeState(["profexp",i,"jobdesignation"],e.target.value)}/>
+                </div>
+                  <div className="secondheaddis-3">
+                      <input type="text" value={companyName}
+                      
+                      onChange={(e)=>changeState(["profexp",i,"companyName"],e.target.value)}/>
+                       |
+                       <input type="text" value={startingdate}
+                       onChange={(e)=>changeState(["profexp",i,"startingdate"],e.target.value)}/>
+                       - 
+                       <input type="text"
+                       value={endingdate}
+                       onChange={(e)=>changeState(["profexp",i,"endingdate"],e.target.value)}/>
+                       </div>
                
                </div>
                 
                <ul>
-               {detailsList.map((val,i)=><li className="licolor-3" key={i}>{val}</li>)}
-                   
-               </ul>
+               {detailsList.map((val,j)=><li className="licolor-3" key={j}>
+                   <textarea 
+                   value={val}
+                   onChange={(e)=>changeState(["profexp",i,"detailsList",j],e.target.value)}/>
+                   </li>)}
+                </ul>
                </>
                 )}
                    )}
@@ -105,12 +176,23 @@ const Raeez = () => {
                      return(
                          <>
                 <div className="second-head-wrapper-3">
-                  <div className="secondhead-3">{course}</div>
-                  <div className="secondheaddis-3">{collegeName} | {graduationyear}</div>
+                  <div className="secondhead-3">
+                      <input type="text" value={course}  className="secondhead-3"
+                      onChange={(e)=>changeState(["Education",i,"course"],e.target.value)}/>
+                      </div>
+                  <div className="secondheaddis-3"><input type="text" value={collegeName} 
+                  onChange={(e)=>changeState(["Education",i,"collegeName"],e.target.value)}/>
+                  | <input type="text" value={graduationyear}  
+                  onChange={(e)=>changeState(["Education",i,"graduationyear"],e.target.value)}/>
+                  </div>
                
                 </div>
                <ul>
-                   <li className="licolor-3">{ academicawards}</li>
+                  {academicawards.map((val,j)=><li className="licolor-3" key={j}>
+                      <textarea
+                      value={val}
+                      onChange={(e)=>changeState(["Education",i,"academicawards",j],e.target.value)}/>
+                    </li>)}
                </ul>
                </>
             )}
@@ -118,7 +200,10 @@ const Raeez = () => {
                
                <div className="firsthead-3">Key Skills</div>
                <ul>
-                   {skills.map((val,i)=><li className="licolor-3" key={i}>{val}</li>)}
+                   {skills.map((val,i)=><li className="licolor-3" key={i}>
+                   <textarea value={val}  
+                   onChange={(e)=>changeState(["skills",i],e.target.value)}/></li>)}
+                  
                </ul>
 
 
