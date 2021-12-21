44409f3a442c2d9cab8a15ff084c0dbb0e838200
Rajasree <rajasreepradhakrishnan@gmail.com>
2021-12-21 18:12:48 +0530

    completed

--- src/Templates/Rajasree/index.jsx
+++ src/Templates/Rajasree/index.jsx
@@ -22,10 +22,23 @@ const Rajasree = () => {
                 startingdate:"January 2016",
                 endingdate:"Present",
                 jobdetails:[
-                    "Implement effective company policies to ensure that all practices comply with labor and employment regulations",
-                    "Increased employee retention rates by managing workplace satisfaction to an over 90% success rate by creating and maintaining a positive work environment",
-                    "Develop targeted outreach practices to increase minority recruitment and ensure compliance with affirmative action policies",
-                    "Monitor scheduled in and out times as well as employee breaks to ensure that proper employment laws are met"]  
+                    {id:1,
+                    value:"Implement effective company policies to ensure that all practices comply with labor and employment regulations"
+                    },
+                    {id:2,
+                        value: "Increased employee retention rates by managing workplace satisfaction to an over 90% success rate by creating and maintaining a positive work environment"
+                        },
+                        {id:3,
+                            value:"Develop targeted outreach practices to increase minority recruitment and ensure compliance with affirmative action policies"
+                            },
+                            {id:4,
+                                value:"Monitor scheduled in and out times as well as employee breaks to ensure that proper employment laws are met"
+                                },
+                    
+                    
+                   
+
+                    ]  
             },
             {
                 job:"Human Resources Associate",
@@ -33,9 +46,15 @@ const Rajasree = () => {
                 startingdate:"March 2015 ",
                 endingdate:"January 2016 ",
                 jobdetails:[
-                    "Implement effective company policies to ensure that all practices comply with labor and employment regulations",
-                    "Increased employee retention rates by managing workplace satisfaction to an over 90% success rate by creating and maintaining a positive work environment",
-                    "Develop targeted outreach practices to increase minority recruitment and ensure compliance with affirmative action policies",
+                    {id:1,
+                        value:"Implement effective company policies to ensure that all practices comply with labor and employment regulations"
+                        },
+                        {id:2,
+                            value: "Increased employee retention rates by managing workplace satisfaction to an over 90% success rate by creating and maintaining a positive work environment"
+                            },
+                            {id:3,
+                                value:"Develop targeted outreach practices to increase minority recruitment and ensure compliance with affirmative action policies"
+                                },
                     ]  
             }
             
@@ -47,17 +66,46 @@ const Rajasree = () => {
                 joiningdate:"September 2007",
                 graduationdate:"May 2011",
                 academicdetails:[
-                    "Academic Awardee of AY 2007-2008"]  
+                    {
+                        id: 1,
+                        value: "Academic Awardee of AY 2007-2008"
+
+                    }
+                   ]  
             }],
         skills:[
-                
-                    "Detail oriented",
-                   " Well-versed in Texas employment law",
-                    "Excellent written and oral communication skills",
-                    "Develops positive workplace relationships"
+           
+            
+            {
+                id:1,
+                value: "Detail oriented"
+            },
+            {
+                id:2,
+                value:" Well-versed in Texas employment law"
+            },
+            {
+                id:3,
+                value:"Excellent written and oral communication skills" 
+            },
+            {
+                id:4,
+                value: "Develops positive workplace relationships"
+            }
+                    
                         ] 
 
     })
+    const getLogo=(name)=>{
+        let nameList = name.split(" ");
+        if(nameList.length<2 || !nameList[1]){
+            return nameList[0][0]
+        }
+        if(!nameList[nameList.length-1]){
+            return nameList[0][0]+nameList[nameList.length -2][0];
+        }
+        return nameList[0][0]+nameList[nameList.length -1][0];
+    }
     const changeState =(keys,value)=>{
         setState((prev)=>{
             return produce(prev,(draft)=>{
@@ -74,6 +122,60 @@ const Rajasree = () => {
                     case 4:
                         draft[keys[0]][keys[1]][keys[2]][keys[3]]=value;
                         break;
+                    case 5:
+                        draft[keys[0]][keys[1]][keys[2]][keys[3]][keys[4]]=value;
+                        break;
+                        default:
+                            break;
+                }
+            })
+        })
+    }
+    const addToList =(keys,i,value)=>{
+        setState((prev)=>{
+            return produce(prev,(draft)=>{
+                switch(keys.length){
+                    case 1:
+                        draft[keys[0]].splice(i,0,value);
+                        break;
+                    case 2:
+                        draft[keys[0]][keys[1]].splice(i,0,value);
+                        break;
+                    case 3:
+                        draft[keys[0]][keys[1]][keys[2]].splice(i,0,value);
+                        break;
+                    case 4:
+                        draft[keys[0]][keys[1]][keys[2]][keys[3]].splice(i,0,value);
+                        break;
+                        default:
+                            break;
+                }
+            })
+        })
+    }
+    const deleteList =(keys,i,value)=>{
+        if(i===0)return
+        setState((prev)=>{
+            return produce(prev,(draft)=>{
+                switch(keys.length){
+                    case 1:
+                        draft[keys[0]].splice(i,1);
+                        draft[keys[0]][i-1].value += value;
+                        break;
+                    case 2:
+                        draft[keys[0]][keys[1]].splice(i,1);
+                        draft[keys[0]][keys[1]][i-1].value += value;
+                        break;
+                    case 3:
+                        draft[keys[0]][keys[1]][keys[2]].splice(i,1);
+                        draft[keys[0]][keys[1]][keys[2]][i-1].value += value;
+
+                        break;
+                    case 4:
+                        draft[keys[0]][keys[1]][keys[2]][keys[3]].splice(i,1);
+                        draft[keys[0]][keys[1]][keys[2]][keys[3]][i-1].value += value;
+
+                        break;
                         default:
                             break;
                 }
@@ -84,7 +186,7 @@ const Rajasree = () => {
     return (
         <div className="main_body4">
             <div className="header_section4">
-                <div className="logo4">{logo}</div>
+                <div className="logo4">{getLogo(name)}</div>
 
                 <div className="namesection">
                 <TextField value={name} className="name4"
@@ -131,6 +233,7 @@ const Rajasree = () => {
                 />
             </div>
             <div className="firsthead4">Professional Experience</div>
+            
             {experience.map(({job,company,startingdate,endingdate,jobdetails},i)=>{
                 return(
                     <>
@@ -143,7 +246,7 @@ const Rajasree = () => {
                     onChange={(e)=>changeState(["experience",i,"job"],e.target.value)}/> */}
                 </div>
             <div className="secondsmallhead4">
-            <TextField value={company} className="secondsmallhead4"
+            <TextField value={company}
                 onChange={(value)=>changeState(["experience",i,"company"],value)}/>
 
             {/* <input type="text" value={company}
@@ -161,11 +264,17 @@ const Rajasree = () => {
                    {/* <input type="text" value={endingdate}
                     onChange={(e)=>changeState(["experience",i,"endingdate"],e.target.value)}/> */}
                    {/* {endingdate} */}
+                     <button onClick={()=>
+                    deleteList(["experience"],i)}
+                     className="close4">X</button>
             </div></div>
             <ul>
-            {jobdetails.map((value,j)=><li className="licolor" key={j}>
+            {jobdetails.map(({value,id},j)=><li className="licolor" key={id}>
             <TextArea value ={value}
-                    onChange={value=>changeState(["experience",i,"jobdetails",j],value)}/>
+                    onChange={value=>changeState(["experience",i,"jobdetails",j,"value"],value)}
+                    addToList={value=>addToList(["experience",i,"jobdetails"],j+1,value)}
+                    deleteList={value=>deleteList(["experience",i,"jobdetails"],j,value)}
+                    length={jobdetails.length}/>
             {/* <textarea value={value}
                     onChange={(e)=>changeState(["experience",i,"jobdetails",j],e.target.value)}/> */}
             </li>)}
@@ -176,6 +285,18 @@ const Rajasree = () => {
 
                 )
             })}
+            <div className="new4">
+            <button onClick={()=>
+            addToList(["experience"],experience.length,{
+                job:"Human Resources Manager",
+                company:"Jim's Widget Factory, Plano, TX",
+                startingdate:"January 2016",
+                endingdate:"Present",
+                jobdetails:[{id:1,
+                     value:"Implement effective company policies to ensure that all practices comply with labor and employment regulations"},]
+
+            })} 
+            >Add new</button></div>
              <div className="firsthead4">Education</div>
                 {education.map(({qualification,joiningdate,graduationdate,academicdetails},i)=>{
                     return(
@@ -201,11 +322,17 @@ const Rajasree = () => {
                     {/* <input type="text" value={graduationdate}
                     onChange={(e)=>changeState(["education",i,"graduationdate"],e.target.value)}/> */}
                     {/* {graduationdate} */}
+                    <button onClick={()=>
+                    deleteList(["education"],i)}
+                     className="close4">X</button>
                </div></div> 
                 <ul>
-                {academicdetails.map((value,j)=><li className="licolor" key={j}>
+                {academicdetails.map(({value,id},j)=><li className="licolor" key={id}>
                 <TextArea value ={value}
-                    onChange={value=>changeState(["education",i,"academicdetails",j],value)}
+                    onChange={value=>changeState(["education",i,"academicdetails",j,"value"],value)}
+                    addToList={value=>addToList(["education",i,"academicdetails"],j+1,value)}
+                    deleteList={value=>deleteList(["education",i,"academicdetails"],j,value)}
+                    length={academicdetails.length}
             // <textarea value={value}
             //         onChange={(e)=>changeState(["education",i,"academicdetails",j],e.target.value)}
                     />
@@ -216,11 +343,26 @@ const Rajasree = () => {
 
 
                  ) })}
+             <div className="new4">
+            <button onClick={()=>
+            addToList(["education"],education.length,{
+                qualification:"Bachelor of Arts in Business",
+                college:"The University of Texas at Dallas ",
+                joiningdate:"September 2007",
+                graduationdate:"May 2011",
+                academicdetails:[
+                    {id:1,value:"Academic Awardee of AY 2007-2008"}]  
+               
+            })} 
+            >Add new</button></div>
             <div className="firsthead4">Key Skills</div>
             <ul>
-            {skills.map((value,i)=><li className="licolor" key={i}>
+            {skills.map(({value,id},i)=><li className="licolor" key={id}>
             <TextArea value ={value}
-                    onChange={value=>changeState(["skills,i"],value)}/>
+                    onChange={(value)=>changeState(["skills",i,"value"],value)}
+                    addToList={value=>addToList(["skills"],i+1,value)}
+                    deleteList={value=>deleteList(["skills"],i,value)}
+                    length={skills.length}/>
             
             {/* <textarea value={value}
                     onChange={(e)=>changeState(["skills",i],e.target.value)}/> */}
@@ -228,6 +370,7 @@ const Rajasree = () => {
                 </li>)}
                 
             </ul> 
+            
             
             </div>
             
