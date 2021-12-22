e211673046e0c08f373d90a7702a8949c76930ee
geethu <geethukallada1@gmail.com>
2021-12-22 15:05:31 +0530

    done

---
+++ src/Templates/Geethu/SecondPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Second.css"
+
+const SecondPage = () => {
+    return (
+        <>
+        <div className='main-container-1'>
+            <div className="header-container-1">
+
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default SecondPage
