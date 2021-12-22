320509304618e9ee168f10221c5e1a481375de53
Raeez98 <raeezmohammed97@gmail.com>
2021-12-22 15:50:07 +0530

    first template completed

---
+++ src/Templates/Raeez/SecondPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SecondPage = () => {
+    return (
+        <div className="main-body3">
+            Raeez
+        </div>
+    )
+}
+
+export default SecondPage
