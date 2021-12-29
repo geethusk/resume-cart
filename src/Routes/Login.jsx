c606042d1bbf005c99ca298198be5319d2457265
faslulfarisa <faslulfarisa5678@gmail.com>
2021-12-29 16:29:33 +0530

    done

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 const Login = () => {
+    const [formData,setFormData] = useState({
+        email:"",
+        password:"",
+    });
+    const{email,password}=formData;
+    const onChange = (key,value)=>{
+        setFormData(prev=>({
+            ...prev,
+            [key]:value
+        }))
+    }
     return (
         <div className="login-container">
             <div className="login-card">
@@ -9,12 +20,18 @@ const Login = () => {
                 </div>
                 <form onSubmit="submit" className='login-form'>
                     <div className="login-section">
+                        <input type="text" className='login-input' alt="email"
+                            value={email}
+                            onChange={(e) =>onChange("email",e.target.value)}
+                        />
                         <div className="login-label">Email</div>
-                        <input type="text" className='login-input' alt="email"/>
                     </div>
                     <div className="login-section">
+                        <input type="text" className='login-input' alt="password"
+                            value={password}
+                            onChange={(e) =>onChange("password",e.target.value)}
+                        />
                         <div className="login-label">Password</div>
-                        <input type="text" className='login-input' alt="password"/>
                         
                     </div>
                     <div className='login-button-section'>
