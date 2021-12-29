f7926193757d5b224774f79290913ae231ae53a9
Vishnu <vishnurnair208@gmail.com>
2021-12-29 10:40:22 +0530

    cleared

--- src/Routes/SignUp.jsx
+++ src/Routes/SignUp.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const SignUp = () => {
     return (
-        <div>
-            sign-up
+        <div className='sign-up-container'>
+            <div className="card-container">
+                <div className="signup">
+                    Sign Up
+                </div>
+                <form action="">
+                    fullname:
+                    <input type="text" className='full-name'/>
+                </form>
+            </div>
         </div>
     )
 }
