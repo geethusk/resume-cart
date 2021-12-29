db98d021be41c4f16a046b2ee29b1d595cb2f380
geethu <geethukallada1@gmail.com>
2021-12-29 15:02:50 +0530

    done

--- src/Routes/Component/HomeDescription.jsx
+++ src/Routes/Component/HomeDescription.jsx
@@ -8,7 +8,7 @@ import Forth from "../Component/4.png"
 const HomeDescription = () => {
     return (
         <div className="home-description-image-section">
-           <h1 className="home-heading">Showcase your potential,<br/>Cut down on writing and<br/>Store your resume online</h1>
+           <h1 className="home-heading">Showcase your potential Cut down on writing and Store your resume online</h1>
             <div className="home-description-section">
                 Easily build a professional-looking resume that showcases your goals and qualifications. Just pick a<br/>template and fill in the blanks. No formatting is needed!
             </div> 
