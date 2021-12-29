f10415ed3e0610f334a1211c47b3497994ecb3e7
faslulfarisa <faslulfarisa5678@gmail.com>
2021-12-29 14:23:50 +0530

    second template

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Login.css'
 const Login = () => {
     return (
         <div className="login-container">
@@ -7,11 +7,21 @@ const Login = () => {
                 <div className="login-heading">
                     Login
                 </div>
-                <form action="">
-                    <label>Email</label>
-                    <input type="text" alt="email"/>
-                    <label>Password</label>
-                    <input type="text" alt="password"/>
+                <form onSubmit="submit" className='login-form'>
+                    <div className="login-section">
+                        <div className="login-label">Email</div>
+                        <input type="text" className='login-input' alt="email"/>
+                    </div>
+                    <div className="login-section">
+                        <div className="login-label">Password</div>
+                        <input type="text" className='login-input' alt="password"/>
+                        
+                    </div>
+                    <div className='login-button-section'>
+                        <div>Forget Password?</div>
+                        <button type="submit" className='login-button'>Login</button>
+                    </div>
+                    <div className='sign-up-page-link'>Not a member?<span className='sign-up-link'> SignUp </span>now</div>
                 </form>
             </div> 
         </div>
