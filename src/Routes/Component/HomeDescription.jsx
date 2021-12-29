65b765648d2a92d3b4932f721e107e55e83791f5
geethu <geethukallada1@gmail.com>
2021-12-29 11:02:53 +0530

    home

---
+++ src/Routes/Component/HomeDescription.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../HomeStyle.css"
+
+const HomeDescription = () => {
+    return (
+        <div>
+           <h1>Resume Definition,<br/>Information and<br/> Professional Help</h1>
+            <div className="home-description-section">
+                A well-written resume job description can offer hiring managers important indications regarding<br/>the type of employee you are and the potential value you can bring to an organization.<br/>
+                Regardless of your qualifications, showing that you have a history of using your knowledge<br/>and skills for other companies should increase your odds of being considered for the position.
+            </div> 
+        </div>
+    )
+}
+
+export default HomeDescription
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import HomeComponent from './Component/HomeComponent'
 import "./HomeStyle.css"
+import "./Component/HomeDescription"
+import HomeDescription from './Component/HomeDescription'
+
 const Home = () => {
     return (
         <div>
             <HomeComponent/>
-            
+            <HomeDescription/>
         </div>
     )
 }
