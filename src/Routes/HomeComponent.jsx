1c66afd5c3af70164c72cf7f7967896c16699c53
Rajasree <rajasreepradhakrishnan@gmail.com>
2021-12-29 10:25:45 +0530

    OK

--- src/Routes/HomeComponent.jsx
+++ src/Routes/HomeComponent.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const HomeComponent = () => {
     return (
-        <div>
+        <div className='mainContainer_home'>
+            <div className='header_home'>
+                <div className='home_icon'>
+                    HOME
+                </div>
+                <div className='resume_home'>
+                    RESUMES
+                </div>
+                <div className='working_home'>
+                    HOW IT WORKS
+                </div>
+                <div className='about_home'>
+                    ABOUT US
+                </div>
+                <div className='feedback_home'>
+                    FEEDBACK
+                </div>
+                <div className='login_home'>
+                    LOGIN
+                </div>
+            </div>
             
         </div>
     )
