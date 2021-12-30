dc0e420ee8dc5e360e31dc2c3abef0499535c9ce
Raeez98 <raeezmohammed97@gmail.com>
2021-12-30 09:19:42 +0530

    steps of how it works

--- src/Routes/HowItWorks.jsx
+++ src/Routes/HowItWorks.jsx
@@ -11,7 +11,7 @@ const HowItWorks = () => {
             </div>
             <div className='procedure-title-3'>Procedure:-</div>
             <div>Resume building is a totally unique and innovative solution to help you reach your career goals. Our service is easy to use, provides all of the necessary tools, and the results really make a difference in how employers see your skills and experience. We are not going to charge you for building resumes in the traditional way, instead you can use all of our matching technology for free and still receive excellent matching results.w you to review what's left out of your mismatch resume in the section titled "my skills that this employer doesn't value" and click on whichever skills you want added back into your mismatch resume..</div>
-            <div className='step-title-3'>Step 1:- Displays Home page</div>
+            <div className='step-title-3'>Step 1:- Navigate to Home page</div>
             <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aspernatur provident laboriosam perspiciatis eveniet fugit quibusdam deleniti consectetur totam autem voluptatum voluptatem fugiat, possimus quasi cum voluptates porro ullam deserunt?</div>
             
         </div>
