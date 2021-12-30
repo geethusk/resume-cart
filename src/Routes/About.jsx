1fa4974f3af9de97cc39895d120d3a48ad92d861
Raeez98 <raeezmohammed97@gmail.com>
2021-12-30 17:34:42 +0530

    about page design

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import "./aboutus.css"
+import resume from "../Routes/hiw-images/resume.svg"
 
 const About = () => {
     return (
         <div className='aboutus-container'>
-            <h1>About Us</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque unde velit minima dicta temporibus quasi expedita accusantium iste ad dolores dolorum, magnam adipisci quo obcaecati blanditiis inventore autem assumenda placeat!</p>
+            <div className='about-head'>
+               <div className="about-main-head">About Us</div>
+               <div className='about-main-description'>Do Everything In Love.</div>
+            </div> 
+            <div className='about-details'>
+               <div className='details-description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam sapiente minus facilis ab maxime corporis atque quasi natus fugiat culpa optio iusto aperiam quis tempora laborum, consequatur expedita nobis alias quaerat error iste quam? Delectus a nostrum saepe possimus inventore et similique enim nesciunt debitis, corrupti quos sunt laudantium, itaque ut hic mollitia soluta sit dignissimos aliquid aliquam facilis iste quod! Aperiam perferendis, ullam ab a beatae voluptatum atque consequatur cum similique omnis dicta optio! Ad cupiditate repellat hic cum unde. Odit mollitia illo beatae, ipsum distinctio magni, in molestiae maxime nihil quia aperiam sed quisquam, porro esse aut rem.</div> 
+            </div> 
+            <div className='about-footer-section'>
+                <img className='about-image-section' src={resume}/>
+                <div className='about-image-description'>Our Products <br/>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, quae debitis quos beatae sequi itaque optio cum ducimus a sapiente! Eaque sequi et ipsa vitae assumenda animi quo voluptate reprehenderit, fugiat tenetur sapiente obcaecati unde vero asperiores! Aspernatur ad illum corporis nulla ut, perferendis fugiat velit voluptate minima temporibus ducimus.</div>
+            </div>
+            
         </div>
     )
 }
