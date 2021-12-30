210d15f60169066364f114a16b99d3fc0fcb74aa
Raeez98 <raeezmohammed97@gmail.com>
2021-12-30 14:46:10 +0530

    aboutus page

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./aboutus.css"
 
 const About = () => {
     return (
-        <div>
-            about
+        <div className='aboutus-container'>
+            <h1>About Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque unde velit minima dicta temporibus quasi expedita accusantium iste ad dolores dolorum, magnam adipisci quo obcaecati blanditiis inventore autem assumenda placeat!</p>
         </div>
     )
 }
