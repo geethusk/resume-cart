73ca30c5420be456010322961bf259c1fd1a2511
geethu <geethukallada1@gmail.com>
2021-12-30 10:19:29 +0530

    navigation

--- src/Routes/Index.jsx
+++ src/Routes/Index.jsx
@@ -5,16 +5,19 @@ import SignUp from "./SignUp"
 import Login from "./Login"
 import View from "./View"
 import HowItWorks from "./HowItWorks"
+import HomeComponent from './Component/HomeComponent'
 
 
 const Index = () => {
     return (
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="sign-up" element={<SignUp/>}/>
-                <Route path="login" element={<Login/>}/>
-                <Route path="view" element={<View/>}/>
-                <Route path="How-It-Works" element={<HowItWorks/>}/>
+                <Route path="" element={<HomeComponent/>}>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="sign-up" element={<SignUp/>}/>
+                    <Route path="login" element={<Login/>}/>
+                    <Route path="view" element={<View/>}/>
+                    <Route path="How-It-Works" element={<HowItWorks/>}/>
+                </Route>
             </Routes>
     )
 }
