cec0a91ed6bd264f920a296eb5d363190b0f2533
Raeez98 <raeezmohammed97@gmail.com>
2021-12-30 10:23:34 +0530

    steps addes in hiw

--- src/Routes/HowItWorks.jsx
+++ src/Routes/HowItWorks.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./works.css"
+import home from "../Routes/hiw-images/home.png"
+
 
 const HowItWorks = () => {
     return (
@@ -9,10 +11,27 @@ const HowItWorks = () => {
                 <div className='main-title3'>HOW IT WORKS</div>
                 <div className='main-title-description-3'>BUILD YOUR RESUME,BUILD YOUR CAREER</div>
             </div>
-            <div className='procedure-title-3'>Procedure:-</div>
-            <div>Resume building is a totally unique and innovative solution to help you reach your career goals. Our service is easy to use, provides all of the necessary tools, and the results really make a difference in how employers see your skills and experience. We are not going to charge you for building resumes in the traditional way, instead you can use all of our matching technology for free and still receive excellent matching results.w you to review what's left out of your mismatch resume in the section titled "my skills that this employer doesn't value" and click on whichever skills you want added back into your mismatch resume..</div>
-            <div className='step-title-3'>Step 1:- Navigate to Home page</div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aspernatur provident laboriosam perspiciatis eveniet fugit quibusdam deleniti consectetur totam autem voluptatum voluptatem fugiat, possimus quasi cum voluptates porro ullam deserunt?</div>
+            <div className='step-container'>
+                 <div className='procedure-title-3'>Procedure:-</div>
+                 <div className='step-info'>Resume building is a totally unique and innovative solution to help you reach your career goals. Our service is easy to use, provides all of the necessary tools, and the results really make a difference in how employers see your skills and experience. We are not going to charge you for building resumes in the traditional way, instead you can use all of our matching technology for free and still receive excellent matching results.w you to review what's left out of your mismatch resume in the section titled "my skills that this employer doesn't value" and click on whichever skills you want added back into your mismatch resume..</div>
+            </div>
+            <div className='step-conatiner'>
+                <div className='step-title-3'>Step 1:- Navigate to Home page</div>
+                <img className='home-image-3' src={home} alt="home-page"/>
+                <div className='step-info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit aspernatur provident laboriosam perspiciatis eveniet fugit quibusdam deleniti consectetur totam autem voluptatum voluptatem fugiat, possimus quasi cum voluptates porro ullam deserunt?</div>
+            </div>
+            <div className='step-conatiner'>
+                <div className='step-title-3'>Step 2:-About US</div>
+                <div className='step-info'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias vero saepe architecto voluptas obcaecati odio! Asperiores sequi reiciendis incidunt sunt temporibus, vitae amet ut deserunt magni, repellendus, quos officia dicta?</div>
+            </div>
+            <div className='step-conatiner'>
+                <div className='step-title-3'>Step 2:-Feedback</div>
+                <div className='step-info'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias vero saepe architecto voluptas obcaecati odio! Asperiores sequi reiciendis incidunt sunt temporibus, vitae amet ut deserunt magni, repellendus, quos officia dicta?</div>
+            </div> 
+            <div className='step-conatiner'>
+                <div className='step-title-3'>Step 2:-Login</div>
+                <div className='step-info'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias vero saepe architecto voluptas obcaecati odio! Asperiores sequi reiciendis incidunt sunt temporibus, vitae amet ut deserunt magni, repellendus, quos officia dicta?</div>
+            </div>   
             
         </div>
     )
