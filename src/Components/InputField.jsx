8d0c9766c9bcf9c9c396a2bbf6c5304e86c9817c
Vishnu <vishnurnair208@gmail.com>
2021-12-31 12:35:44 +0530

    31 dec final commit

--- src/Components/InputField.jsx
+++ src/Components/InputField.jsx
@@ -3,9 +3,9 @@ import React, { useState } from 'react'
 const InputField = ({value,onChange,label,type="text",error,isFormSubmitted=false}) => {
     const [blured,setBlured] = useState(false)
     return (
-        
+        <>
             <div className="input-area">
-                        <input type="text" className='sign-up-input'
+                        <input type="text" className='sign-up-login-input'
                             type={type}
                             value={value}     
                             onChange={(e)=>{
@@ -20,10 +20,12 @@ const InputField = ({value,onChange,label,type="text",error,isFormSubmitted=fals
                         <div className={` label ${value ?  "filled-label": "" } `}>
                             {label}
                         </div>
+             </div>
+
                         {error && (blured || isFormSubmitted) && <div className="text-field-error-text">
                                  {error}
                            </div>}
-            </div>
+            </>
         
     )
 }
