8f4712858e62bc3e9d5d4d6bcdcf4fb2650f4002
Raeez98 <raeezmohammed97@gmail.com>
2022-01-03 16:44:30 +0530

    network and connection lost page designs

--- src/Routes/Network.jsx
+++ src/Routes/Network.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./networkerror.css"
+import networkerror from "../Routes/hiw-images/network-connection.jpg"
 
 const Network = () => {
     return (
-        <div>
-            Network Error
+        <div className='network-error'>
+            <div className='network-error-sub'>
+                <img className='networkerror-image' src={networkerror}/>
+                <div className='network-head'>Connection Lost!</div>
+                <div className='network-head2'>Looks likes the page you are trying to visit does not exist.</div>
+                <div className='network-error-description'>Please Check the URL and try again..</div>
+           
+            </div>
         </div>
     )
 }
