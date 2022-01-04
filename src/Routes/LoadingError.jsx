3c3c2338ed2295b68400cfe30cfa40090b3ca935
Raeez98 <raeezmohammed97@gmail.com>
2022-01-04 12:42:40 +0530

    buttons fixing for connection page

--- src/Routes/LoadingError.jsx
+++ src/Routes/LoadingError.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import "./loadingpage.css"
 import loadingpageimage from  "../Routes/hiw-images/loadingpage.png"
+import { useNavigate } from 'react-router-dom'
 
 const LoadingError = () => {
+   
     return (
         <div className='loadingpage-container'>
             <div className='loadingpage-sub-container'>
             <div className='loadingpage-head'>Loading...</div>
             <img src ={ loadingpageimage }/>
+               
             </div>
         </div>
     )
