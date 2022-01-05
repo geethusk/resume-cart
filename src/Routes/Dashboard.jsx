18de1104632212e3f24b9015527550a53f94f390
faslulfarisa <faslulfarisa5678@gmail.com>
2022-01-05 10:30:40 +0530

    first commit 5th jan

--- src/Routes/Dashboard.jsx
+++ src/Routes/Dashboard.jsx
@@ -2,7 +2,23 @@ import { useContext} from 'react'
 import './Dashboard.css'
 import profilePic from '../Templates/Faslu/images/photo.jpg'
 import { UserContext } from '../Context/UserContext'
+import { useState } from 'react/cjs/react.development';
+
 const Dashboard = () => {
+    const[password,setPassword] = useState({
+        oldpassword:"",
+        newpassword:"",
+        confirmpassword:""
+    })
+    const{oldpassword,newpassword,confirmpassword}=password
+    const onChange = (key,value)=>{
+        setPassword(prev=>({
+            ...prev,
+            [key]:value
+        }))
+    }
+    // console.log(oldpassword);
+
     const { userData} = useContext(UserContext)
     return (
     <div className='dashboard-container'>
@@ -15,19 +31,35 @@ const Dashboard = () => {
         </div>
         <div className="dashboard-right-section">
             <div className="dashboard-section">
-                 <label for ="dashboard-password">Confirm Password</label>
+                 <label for ="dashboard-password">Old Password</label>
                  <div className="dashboard-input">
-                     <input type="text"/><br/></div>
+                     <input type="text" 
+                        // value={oldpassword}
+                        onChange={
+                            (e)=>{onChange("oldpassword",e.target.value)}
+                        }  
+                     /><br/></div>
              </div>
              <div className="dashboard-section">
                  <label for ="dashboard-password">New Password</label>
                  <div className="dashboard-input">
-                     <input type="text"/><br/></div>
+                     <input type="text"
+                        // value={newpassword}
+                        onChange={
+                            (e)=>{onChange("newpassword",e.target.value)}
+                        } 
+                     /><br/></div>
              </div>
              <div className="dashboard-section">
                  <label for ="dashboard-password">Confirm New Password</label>
                  <div className="dashboard-input">
-                     <input type="text"/><br/></div>
+                     <input type="text"
+                        // value={confirmpassword}
+                        onChange={
+                            (e)=>{onChange("confirmpassword",e.target.value)}
+                        } 
+
+                     /><br/></div>
             </div>
             <div>
             <button className='dashboard-button'>UPDATE</button>
