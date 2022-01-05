417362b6780c74574e046cc4d5f18e5e62e161c3
geethu <geethukallada1@gmail.com>
2022-01-05 10:45:50 +0530

    admin

---
+++ src/Routes/Admin.jsx
@@ -0,0 +1,16 @@
+import {useNavigate, useState} from 'react'
+import "../Routes/HomeStyle.css"
+
+
+const Admin = () => {
+    const navigate=useNavigate();
+    const [isLoggedIn,setIsLoggedIn]=useState(false);
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Admin
