420d09998d6d8ae907fb5af204c6e3e41a3956bd
Rajasree <rajasreepradhakrishnan@gmail.com>
2022-01-07 11:59:42 +0530

    👍

--- src/Routes/Component/Form.jsx
+++ src/Routes/Component/Form.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import postData from '../../services/postdata'
 import "../Component/form.css"
 const Form = ({setFormVisibility}) => {
+    const [image,setImage]=useState(null)
+    const [upload,setUpload]=useState(false)
+    console.log(image);
+
+    const uploadTemplate = async ()=>{ 
+        const data = new FormData();
+        data.append('file', image)
+        
+            fetch('http://192.168.1.66:5000/api/v1/template-upload', {
+            method: 'POST',
+            headers: {
+                'authorization': localStorage.getItem('token') ? localStorage.getItem('token'): ""
+              },
+              body: data
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        }
+         
     return (
         <div className='form_home_container'>
         <div className='form_home'>
@@ -26,14 +46,17 @@ const Form = ({setFormVisibility}) => {
                 </div>
                 <div className='template_demand'>
                 <label className="input_form_label" htmlFor="">Demo</label>
-                
-                <input type="file"/>
+                {!upload && 
+                <input type="file" id="template_image" onChange={(e)=>{setImage(e.target.files[0])
+                setUpload(true)}
+                }/>}
+                {upload && <div className="backend-upload" onClick={()=>{uploadTemplate(); setUpload(false)}}>DONE</div>}
                 </div>
                 <div className='template_demand'>
                 <label className="input_form_label" htmlFor="">Image Url</label>
                 <input type="text" placeholder="Url"/>
                 </div>
-
+                <button onClick={()=>{}}>OK</button>
             </form>
             <div className='form-close-button' onClick={()=>setFormVisibility(false)}>X</div>
         </div>
